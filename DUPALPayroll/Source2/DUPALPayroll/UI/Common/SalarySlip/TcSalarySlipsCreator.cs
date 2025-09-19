e2a22ed69ce66201f31c1606de60877ea35b3845
Ruchira Bomiriya <ruboware@gmail.com>
2025-09-19 16:22:54 +0530

    Revert "DUPAL Address Changed!"
    This reverts commit c9a979ef885569a50b076b9d56876696de9b712d.

--- DUPALPayroll/Source2/DUPALPayroll/UI/Common/SalarySlip/TcSalarySlipsCreator.cs
+++ DUPALPayroll/Source2/DUPALPayroll/UI/Common/SalarySlip/TcSalarySlipsCreator.cs
@@ -190,9 +190,9 @@ namespace DUPALPayroll.UI.Common.SalarySlip
         paragraph.Format.Alignment = ParagraphAlignment.Left;
 
         // Address
-        paragraph = companyAddressFrame.AddParagraph("2nd Floor, 299/2 Union Place");
+        paragraph = companyAddressFrame.AddParagraph("33 1/1, Skelton Gardens,");
         paragraph.AddLineBreak();
-        paragraph.AddText("Colombo 2,i Sri Lanka");
+        paragraph.AddText("Colombo 5, Sri Lanka");
         SetSmallFontToParagraph(paragraph);
         paragraph.Format.Alignment = ParagraphAlignment.Left;
 
